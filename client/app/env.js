bb80452ed695cc3f05854e480b02a17a27a6015f
ShereneC <spyhopper@cableone.net>
2021-10-05 17:13:50 -0600

    init

---
+++ client/app/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'sc-codeworks.us.auth0.com'
+export const audience = 'https://nativetracker.com'
+export const clientId = 'Aub8QdJZ6NMtsjgmI5Bcf2WvL4Nrf3YA'
